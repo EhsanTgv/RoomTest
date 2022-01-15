45fc9e7ee9a0c2291da8812437f49ea19c777879
EhsanTgv <EhsanTgv@gmail.com>
2022-01-15 19:24:08 +0300

    Insert Data to Database

--- app/src/main/java/com/taghavi/roomtest/fragments/add/AddFragment.kt
+++ app/src/main/java/com/taghavi/roomtest/fragments/add/AddFragment.kt
@@ -1,20 +1,54 @@
 package com.taghavi.roomtest.fragments.add
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextUtils
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.taghavi.roomtest.data.User
+import com.taghavi.roomtest.data.UserViewModel
 import com.taghavi.roomtest.databinding.FragmentAddBinding
 
 class AddFragment : Fragment() {
     private lateinit var binding: FragmentAddBinding
+    private lateinit var viewModel: UserViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
         binding = FragmentAddBinding.inflate(layoutInflater, container, false)
 
+        viewModel = ViewModelProvider(this).get(UserViewModel::class.java)
+
+        binding.addButton.setOnClickListener {
+            insertDataToDatabase()
+        }
+
         return binding.root
+    }
+
+    private fun insertDataToDatabase() {
+        val firstName = binding.firstName.text.toString()
+        val lastName = binding.lastName.text.toString()
+        val age = binding.age.text
+
+        if (inputCheck(firstName, lastName, age)) {
+            val user = User(0, firstName, lastName, age.toString().toInt())
+            viewModel.addUser(user)
+            Toast.makeText(requireContext(), "Successfully added!", Toast.LENGTH_SHORT).show()
+            findNavController().popBackStack()
+        } else {
+            Toast.makeText(requireContext(), "Please fill out all fields", Toast.LENGTH_SHORT)
+                .show()
+        }
+    }
+
+    private fun inputCheck(firstName: String, lastName: String, age: Editable): Boolean {
+        return !(TextUtils.isEmpty(firstName) && TextUtils.isEmpty(lastName) && age.isEmpty())
     }
 }
