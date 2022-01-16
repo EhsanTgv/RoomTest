3ba4719c96bfd4f71ae0d413588e0d3a661e3380
EhsanTgv <EhsanTgv@gmail.com>
2022-01-16 20:46:06 +0300

    Read Data from Database

---
+++ app/src/main/java/com/taghavi/roomtest/fragments/list/ListAdapter.kt
@@ -0,0 +1,28 @@
+package com.taghavi.roomtest.fragments.list
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.taghavi.roomtest.data.User
+import com.taghavi.roomtest.databinding.ItemRowBinding
+
+class ListAdapter(private val userList: List<User>) :
+    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder =
+        ItemViewHolder(ItemRowBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+
+    override fun getItemCount(): Int = userList.size
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val viewHolder = holder as ItemViewHolder
+        with(viewHolder.viewBinding) {
+            id.text = userList[position].id.toString()
+            firstName.text = userList[position].firstName
+            lastName.text = userList[position].lastName
+            age.text = userList[position].age.toString()
+        }
+    }
+
+    class ItemViewHolder(var viewBinding: ItemRowBinding) :
+        RecyclerView.ViewHolder(viewBinding.root)
+}
--- app/src/main/java/com/taghavi/roomtest/fragments/list/ListFragment.kt
+++ app/src/main/java/com/taghavi/roomtest/fragments/list/ListFragment.kt
@@ -5,16 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.taghavi.roomtest.data.UserViewModel
 import com.taghavi.roomtest.databinding.FragmentListBinding
 
 class ListFragment : Fragment() {
     private lateinit var binding: FragmentListBinding
+    private lateinit var viewModel: UserViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
         binding = FragmentListBinding.inflate(layoutInflater, container, false)
+
+        viewModel = ViewModelProvider(this).get(UserViewModel::class.java)
+        viewModel.readAllData.observe(viewLifecycleOwner, Observer { users ->
+            binding.recyclerView.adapter = ListAdapter(users)
+        })
+
+        binding.recyclerView.layoutManager = LinearLayoutManager(requireContext())
 
         binding.floatingActionButton.setOnClickListener {
             findNavController().navigate(ListFragmentDirections.actionListFragmentToAddFragment())
